8b3ac48d62ffc6a8ba7673fd5b9e79528d6a8208
Abhijit Apte <abhijitapte@Abhijits-MacBook-Air.local>
2016-01-30 11:05:52 +0530

    Readying for pushing to remote origin

---
+++ SimpleCompiler/MicroScanner.cpp
@@ -0,0 +1,9 @@
+//
+//  MicroScanner.cpp
+//  SimpleCompiler
+//
+//  Created by Abhijit Apte on 26/01/16.
+//  Copyright © 2016 Abhijit Apte. All rights reserved.
+//
+
+#include "MicroScanner.hpp"
---
+++ SimpleCompiler/MicroScanner.hpp
@@ -0,0 +1,14 @@
+//
+//  MicroScanner.hpp
+//  SimpleCompiler
+//
+//  Created by Abhijit Apte on 26/01/16.
+//  Copyright © 2016 Abhijit Apte. All rights reserved.
+//
+
+#ifndef MicroScanner_hpp
+#define MicroScanner_hpp
+
+#include <stdio.h>
+
+#endif /* MicroScanner_hpp */
